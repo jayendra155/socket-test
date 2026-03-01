72bd6d786a0177ab4dcc164f4cc5df5be36b2277
Jayendra <jayendra@Mac.local>
2026-03-01 07:27:36 +0530

    Setup checkstyle, build info and formatting

---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.3"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("checkstyle")
+	id("com.diffplug.spotless") version "7.0.2"
+}
+
+springBoot {
+	buildInfo()
+}
+
+checkstyle {
+	toolVersion = "10.21.3"
+	config = resources.text.fromArchiveEntry(configurations.checkstyle, "google_checks.xml")
+    maxErrors = 0
+    maxWarnings = 0
+}
+
+spotless {
+	java {
+		target("src/*/java/**/*.java")
+		trimTrailingWhitespace()
+		leadingTabsToSpaces(4)
+		endWithNewline()
+	}
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["datasourceMicrometerVersion"] = "2.1.1"
+extra["springCloudVersion"] = "2025.1.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-h2console")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("net.ttddyy.observation:datasource-micrometer-spring-boot")
+	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+	runtimeOnly("io.r2dbc:r2dbc-h2")
+	runtimeOnly("io.r2dbc:r2dbc-mssql:1.0.0.RELEASE")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-r2dbc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-websocket-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("net.ttddyy.observation:datasource-micrometer-bom:${property("datasourceMicrometerVersion")}")
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
